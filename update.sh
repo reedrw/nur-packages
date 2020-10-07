cdfad423af7f9d3aa0f543f3e6d747d806e45f24
Reed <mrnitsuj1@gmail.com>
2020-10-07 19:52:29 -0400

    just testing

--- update.sh
+++ update.sh
@@ -1,20 +1,23 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash
 
-# number-versioned packages. Use nix-update
-while read -r i; do
-  nix-update "$i" || exit 1
-done << EOF
-  r2mod_cli
-  scientifica
-EOF
+## number-versioned packages. Use nix-update
+#while read -r i; do
+#  nix-update "$i" || exit 1
+#done << EOF
+#  r2mod_cli
+#  scientifica
+#EOF
+#
+## other. Run their update.sh
+#while read -r i; do
+#  pushd pkgs/"$i"/
+#  ./update.sh
+#  popd
+#done << EOF
+#  artwiz-lemon
+#EOF
+#
 
-# other. Run their update.sh
-while read -r i; do
-  pushd pkgs/"$i"/
-  ./update.sh
-  popd
-done << EOF
-  artwiz-lemon
-EOF
-
+# just testing this
+nix-prefetch '(import ./. {}).r2mod_cli'
