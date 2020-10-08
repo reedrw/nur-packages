884fa1c3b8c7eda8dad37ee85ccdd5c18a503b5b
Reed <mrnitsuj1@gmail.com>
2020-10-07 21:48:34 -0400

    this should hopefully now actually work please

--- shell.nix
+++ shell.nix
@@ -2,18 +2,22 @@ with import <nixpkgs> {};
 
 let
 
-nix-prefetch-master = pkgs.fetchFromGitHub {
-  owner = "msteen";
-  repo = "nix-prefetch";
-  rev = "6bda3ef3862173b3dabff3c2668add795f982572";
-  sha256 = "1qw7ifc4syh8xprfcnj4kc7brx4x7bd0584sskgc1b3cpjnq5qs2";
-};
-nix-prefetch-release = "${nix-prefetch-master}/release.nix";
+nix-prefetch-fixed = pkgs.nix-prefetch.overrideAttrs (
+  old: rec {
+    src = pkgs.fetchFromGitHub {
+      owner = "msteen";
+      repo = "nix-prefetch";
+      rev = "6bda3ef3862173b3dabff3c2668add795f982572";
+      sha256 = "1qw7ifc4syh8xprfcnj4kc7brx4x7bd0584sskgc1b3cpjnq5qs2";
+    };
+    patches = [];
+  }
+);
 
 nix-update-fixed = pkgs.nix-update.overrideAttrs (
   old: rec {
     makeWrapperArgs = [
-      "--prefix" "PATH" ":" (lib.makeBinPath [nix nix-prefetch-release])
+      "--prefix" "PATH" ":" (lib.makeBinPath [nix nix-prefetch-fixed])
     ];
   }
 );
@@ -24,6 +28,7 @@ pkgs.mkShell {
 
   buildInputs = [
     nix-update-fixed
+    nix-prefetch-fixed
     pkgs.nix-prefetch-github
   ];
 
