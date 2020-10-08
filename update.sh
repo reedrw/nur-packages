02fa86ff27bd09b26df4b01167ce5384d312fd80
Reed <mrnitsuj1@gmail.com>
2020-10-07 21:25:40 -0400

    ACTUALLY FIXED????

--- update.sh
+++ update.sh
@@ -1,23 +1,20 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash
 
-## number-versioned packages. Use nix-update
-#while read -r i; do
-#  nix-update "$i" || exit 1
-#done << EOF
-#  r2mod_cli
-#  scientifica
-#EOF
-#
-## other. Run their update.sh
-#while read -r i; do
-#  pushd pkgs/"$i"/
-#  ./update.sh
-#  popd
-#done << EOF
-#  artwiz-lemon
-#EOF
-#
+# number-versioned packages. Use nix-update
+while read -r i; do
+  nix-update "$i" || exit 1
+done << EOF
+  r2mod_cli
+  scientifica
+EOF
 
-# just testing this
-nix-prefetch hello --output nix -vv
+# other. Run their update.sh
+while read -r i; do
+  pushd pkgs/"$i"/
+  ./update.sh
+  popd
+done << EOF
+  artwiz-lemon
+EOF
+
