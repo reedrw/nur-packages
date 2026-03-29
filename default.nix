f7d8d6b71ed848ab13a411256d679453530f9283
Reed <mrnitsuj1@gmail.com>
2026-03-29 08:51:43 -0400

    genshin-account-switcher: drop

--- default.nix
+++ default.nix
@@ -16,7 +16,6 @@
 
   artwiz-lemon = pkgs.callPackage ./pkgs/artwiz-lemon { };
   bitwarden-rofi = pkgs.callPackage ./pkgs/bitwarden-rofi { };
-  genshin-account-switcher = pkgs.callPackage ./pkgs/genshin-account-switcher { };
   jkps = pkgs.callPackage ./pkgs/jkps { };
   rctpm = pkgs.callPackage ./pkgs/rctpm { };
   shairport-sync-metadata-reader = pkgs.callPackage ./pkgs/shairport-sync-metadata-reader { };
